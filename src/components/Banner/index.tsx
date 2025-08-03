cf2b34ba12efb1a934979fb4a9a0814e0965cc30
Afrânio Nunes Dantas <afraniodantas0224@gmail.com>
2025-08-02 22:25:13 -0300

    feat: add component footer

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,16 +1,32 @@
+import classNames from "classnames";
 import styles from "./Banner.module.css";
 
 type BannerProps = {
   src: string;
   alt: string;
+  variant?: "default" | "combo";
+  text1?: string;
+  text2?: string;
 };
 
-const Banner = ({ src, alt }: BannerProps) => {
+const Banner = ({
+  src,
+  alt,
+  text1,
+  text2,
+  variant = "default",
+}: BannerProps) => {
+  const classMap = {
+    default: "",
+    combo: styles.combo,
+  };
+
   return (
-    <div className={styles.container}>
+    <div className={classNames(styles.container, classMap[variant])}>
       <img src={src} alt={alt} className={styles.banner} />
       <h1>
-        Onde a realidade <span>encontra a fantasia!</span>
+        {text1}
+        <span>{text2}</span>
       </h1>
     </div>
   );
